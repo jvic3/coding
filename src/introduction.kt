8a35413bd18aaae293199c35ffae25db059a1ff5
jvic3 <junemumo70@gmail.comgit commit --m initial>
2024-03-11 13:10:42 +0300

    initial commit

---
+++ src/introduction.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("Juliana")
+    println(20)
+    println(10+10)
+    println("eMobilis")
+    println("June")
+    println("name")
+}
---
+++ src/variables.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var firstname = "Susan"
+    val pi = 3.14
+    var lastname = "Chomba"
+    var school = "eMobilis"
+
+    println(firstname + " " + lastname)
+    println("My firstname is "+" "+firstname+" "+" i study that")
+
+
+    
+
+
+}
